5d28ed8276e8b980af6fd4ce45a128da3861a6ac
Maryna Kidun <marykidun@gmail.com>
2022-10-28 00:13:29 +0300

    Implemented all tasks

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,13 @@
 package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
-    public void isLeapYear(int year) {
+    public static void isLeapYear(int year) {
+        String result;
+        result = ((year % 4 == 0 && year % 100 != 0) ? "leap" : (year % 400 == 0) ? "leap" : "not leap");
+           System.out.println(result);
+
+        }
+    public static void main(String[] args) {
+        isLeapYear(2019);
     }
 }
